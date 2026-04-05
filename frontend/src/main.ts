c2623f061bb2419acde84e8bf6b80b43e44c7a92
ErikJermanis <erik55jermanis@gmail.com>
2026-04-05 13:39:56 +0200

    yeet

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,3 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div class="text-blue-600">hehe</div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ justfile
@@ -0,0 +1,7 @@
+run:
+  cd frontend && npm run dev
+
+yeet:
+  git add .
+  git commit -m "yeet"
+  git push
